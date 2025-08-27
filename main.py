3c56f82aefd8b4b435be72131fb58a4968e648b0
dmytrolutsyk <49659495+dmytrolutsyk@users.noreply.github.com>
2025-08-27 12:14:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import os
 from dotenv import load_dotenv
 import requests
 import msal
+from Brevo import create_user, init_brevo
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
 
 processed_users_file_name = "processed_users.json"
 processed_users = {}
+
+
 def getNewUsers():
-    print('Beggining ...')
+    print('Récupération des utilisateurs ...')
     try:
         load_dotenv()
         tenant_id = os.getenv("TENANT_ID")
@@ -35,15 +40,15 @@ def getNewUsers():
         token = result["access_token"]
 
         # Appel Graph API
-        #url = f"https://graph.microsoft.com/v1.0/users?$select=id,displayName,mail,userPrincipalName,userType,department,manager,officeLocation,companyName,jobTitle,createdDateTime&$filter=createdDateTime ge {since_iso}"
-        url = f"https://graph.microsoft.com/v1.0/users?$select=id,displayName,mail,userPrincipalName,userType,department,manager,officeLocation,companyName,jobTitle,createdDateTime&$filter=createdDateTime ge {since_iso} and userType eq 'Member'"
+        # url = f"https://graph.microsoft.com/v1.0/users?$select=id,displayName,mail,userPrincipalName,userType,department,manager,officeLocation,companyName,jobTitle,createdDateTime&$filter=createdDateTime ge {since_iso}"
+        url = f"https://graph.microsoft.com/v1.0/users?$select=id,displayName,givenName,surname,mobilePhone,mail,userPrincipalName,userType,department,manager,officeLocation,companyName,jobTitle,createdDateTime&$filter=createdDateTime ge {since_iso} and userType eq 'Member'"
         headers = {"Authorization": f"Bearer {token}"}
         resp = requests.get(url, headers=headers)
 
         if resp.status_code != 200:
             raise Exception(f"Erreur Graph API : {resp.text}")
 
-        return resp.json()   # retourne le dict Python directement
+        return resp.json()  # retourne le dict Python directement
 
     except Exception as e:
         print(f"Erreur : {str(e)}")
@@ -64,30 +69,47 @@ def getProcessedUsers():
     else:
         return {}
 
+
 def updateProcessedUsers(old_processed_users):
     with open(processed_users_file_name, "w") as file:
         json.dump(old_processed_users, file, indent=4)
+
+
+def create_user_in_brevo(api_instance, email, frist_name, last_name, phone):
+    attributes = {
+        "FIRSTNAME": frist_name,
+        "LASTNAME": last_name,
+        "SMS": phone
+    }
+    list_ids = [423]
+    create_user(api_instance, email, attributes, list_ids)
+
 
 if __name__ == "__main__":
     # Récupération des utilisateurs AD rajoutés sur les 24h dernières heures
     data = getNewUsers()
     # Ouverture du fichier des utilisateurs déjà traités
     old_processed_users = getProcessedUsers()
-    #print(f"Processed users: {old_processed_users}")
+    # print(f"Processed users: {old_processed_users}")
 
     # Itération sur le dictonnaire
     '''for cle, valeur in old_processed_users.items():
         print(f"La clé est : {cle} et la valeur est : {valeur}")
     print("fin de la boucle des users traités")'''
-    #print("Data",data)
+    # print("Data",data)
+    api_instance = init_brevo()
 
-    #Itération sur les nouveaux utilisateurs
+    # Itération sur les nouveaux utilisateurs
     for user in data.get("value", []):
-        print(f"User: {user.get('displayName')} - userPrincipalName: {user.get('userPrincipalName')} - createdDateTime: {user.get('createdDateTime')} - jobTitle: {user.get('jobTitle')}- officeLocation:{user.get('officeLocation')}- companyName:{user.get('companyName')} - userType:{user.get('userType')} - manager:{user.get('manager')}")
+        print(
+            f"User: {user.get('displayName')} - givenName: {user.get('givenName')} - surname: {user.get('surname')} - mobilePhone: {user.get('mobilePhone')} - userPrincipalName: {user.get('userPrincipalName')} - createdDateTime: {user.get('createdDateTime')} - jobTitle: {user.get('jobTitle')}- officeLocation:{user.get('officeLocation')}- companyName:{user.get('companyName')} - userType:{user.get('userType')} - manager:{user.get('manager')}")
         processed_user = user.copy()
         processed_user['processed'] = True
-        if user.get('id') not in old_processed_users:
+        if user.get('id') not in old_processed_users:  # Vérification si l'utilisateur n'est pas déjà traité
             old_processed_users[processed_user['id']] = processed_user
-            updateProcessedUsers(old_processed_users) # Mise à jour du fichier des users traités
+            create_user_in_brevo(api_instance,user.get('mail'),user.get('givenName'), user.get('surname'), user.get('mobilePhone'))
+            updateProcessedUsers(old_processed_users)  # Mise à jour du fichier des users traités
+            exit(0)
         else:
             print(f"L'utilisateur {user.get('displayName')} existe déjà.")
+
